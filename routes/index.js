7c61a223e0b2b9852d24280186d0083610e8bb59
Jacques Smulders <jacqsmulders@gmail.com>
2017-07-19 11:08:07 +0200

    Add files via upload
    Added template for using dbs insert and find

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,14 @@ router.post('/dashboard',function (req,res,next) {
     res.render('index',{ title: 'dashboard',username:req.body.username,password: req.body.password });
 });
 
+router.get('/test-insert', function(req, res, next) {
+    var value = {_id: 'emp_id_123', name: 'John', surname: 'Doe', password: 'BushDid911', password_date: '21-07-2017', email: 'johndoe@kpmg.com', role: 'employee', employment_length: '3', skill: ['MS Office', 'Python']} ;
+
+    dbs.insertEmployee(value) ;
+});
+
+router.get('/test-find', function(req, res, next) {
+    dbs.findEmployee('emp_id_123') ;
+});
 
 module.exports = router;

18505348b57beb729c47d357d2197a27f60930aa
MrDaubinet <daubinet.jordan@gmail.com>
2017-07-23 15:31:52 +0200

    Created the resource algorithm in the new class "Resource-Alogation-Algorithm and its corresponding index.js code

--- database/Resource-Alocation-Algorithm.js
+++ database/Resource-Alocation-Algorithm.js
@@ -3,11 +3,8 @@
  */
 var exports = module.exports = {} ;
 
-var mongoose = require('mongoose') ;
 var schemas = require('.././database/schemas.js') ;
 var dbs = require('.././database/dbs.js') ;
-var generator = require('generate-password');
-var connection = require('.././database/connect.js') ;
 
 exports.get_unallocated_users = function(number, callback) {
   console.log("unallocated users requested");
@@ -18,123 +15,12 @@ exports.get_unallocated_users = function(number, callback) {
             console.log("Error finding User.");
         }
         else if (!result) {
-            console.log("User not found.");
+            console.log("employee not found.");
         }
         else {
-            console.log("User found.");
+            console.log("employee found.");
             //console.log(JSON.stringify(result));
             return callback(result);
         }
     }).limit(number);
-};
-
-//A function to statically create employees into the db
-exports.create_test_employees = function() {
-    console.log("creating a user");
-
-    var rand_password = generator.generate({
-        length: 10,
-        numbers: true,
-        symbols: true,
-        uppercase: true
-    });
-
-    var today = new Date();
-    var enc_pass = dbs.encrypt(rand_password, function (enc_pass) {
-        var emp = {
-            _id: 1,
-            name: "employee1",
-            surname: "test",
-            password: enc_pass,
-            password_date: today,
-            email: "employee1@gmail.com",
-            role: "System test",
-            employment_length: 1,
-            skill: [],
-            current_projects: [],
-            past_projects: []
-        };
-
-        var emp2 = {
-            _id: 2,
-            name: "employee2",
-            surname: "test",
-            password: enc_pass,
-            password_date: today,
-            email: "employee2@gmail.com",
-            role: "System test",
-            employment_length: 1,
-            skill: [],
-            current_projects: [],
-            past_projects: []
-        };
-
-        var emp3 = {
-            _id: 3,
-            name: "employee3",
-            surname: "test",
-            password: enc_pass,
-            password_date: today,
-            email: "employee3@gmail.com",
-            role: "System test",
-            employment_length: 1,
-            skill: [],
-            current_projects: [],
-            past_projects: []
-        };
-
-        var emp4 = {
-            _id: 4,
-            name: "employee4",
-            surname: "test",
-            password: enc_pass,
-            password_date: today,
-            email: "employee4@gmail.com",
-            role: "System test",
-            employment_length: 1,
-            skill: [],
-            current_projects: [],
-            past_projects: []
-        };
-
-        var emp5 = {
-            _id: 5,
-            name: "employee5",
-            surname: "test",
-            password: enc_pass,
-            password_date: today,
-            email: "employee5@gmail.com",
-            role: "System test",
-            employment_length: 1,
-            skill: [],
-            current_projects: [],
-            past_projects: []
-        };
-        //dbs.remove({name: "Testy"});  /*THIS deletes all previous users in the db*/
-        dbs.insertUser(emp);
-        dbs.insertUser(emp2);
-        dbs.insertUser(emp3);
-        dbs.insertUser(emp4);
-        dbs.insertUser(emp5);
-        console.log("employees added to data base")
-    });
-};
-
-//A function to display the db in the terminal
-exports.view_employees = function()
-{
-    console.log("viewing all employees in user db");
-    var user = schemas.user;
-    user.find(function (err, result) {
-        if (err) {
-            console.log("Error displaying users.");
-        }
-        else if (!result) {
-            console.log("database is empty.");
-        }
-        else {
-            console.log("employees found");
-            console.log(JSON.stringify(result));
-        }
-    });
 };
--- database/dbs.js
+++ database/dbs.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose') ;
 var schemas = require('.././database/schemas.js') ;
 var connection = require('.././database/connect.js') ;
 var bcrypt = require('bcrypt') ;
+var generator = require('generate-password');
 
 exports.insertUser = function(_json) {
 
@@ -21,7 +22,7 @@ exports.insertUser = function(_json) {
         }
     }) ;
 
-}
+};
 
 //other search templates could be added
 
@@ -41,7 +42,7 @@ exports.findUser = function(Id, callback) {
             return callback(doc) ;
         }
     });
-}
+};
 
 exports.insertProject = function(_json) {
 
@@ -58,7 +59,7 @@ exports.insertProject = function(_json) {
             console.log("Successfully inserted project.") ;
         }
     });
-}
+};
 
 exports.findProject = function(Id, callback) {
 
@@ -76,7 +77,7 @@ exports.findProject = function(Id, callback) {
             return callback(doc) ;
         }
     });
-}
+};
 
 // exports.encrypt = function(value, callback) {
 //     return bcrypt.hashSync(value, 10)
@@ -87,11 +88,14 @@ exports.encrypt = function(value, callback) {
     bcrypt.hash(value, 10, function(err, hash) {
         return callback(hash) ;
     });
-}
+};
 
 exports.authenticate = function(user_id, password, callback) {
-    var user = module.exports.findUser(user_id, function(user) {
-        var hash = user.password ;
+    console.log("Authenticate Running with user_id: "+user_id+" and password : "+password); //must remove
+    module.exports.findUser(user_id, function(user) {
+        console.log("User details "+user);  //must remove
+        var hash = user.password;
+        console.log("Hashed password found is : "+hash);
         bcrypt.compare(password, hash, function (err, res) {
             if (err) {
                 console.log("Authentication error.") ;
@@ -101,10 +105,10 @@ exports.authenticate = function(user_id, password, callback) {
             return callback(res);
         });
     })
-}
+};
 
 exports.get_role = function(user_id, callback) {
-    var user = module.exports.findUser(user_id, function(user) {
+    module.exports.findUser(user_id, function(user) {
         if (!user) {
             console.log("User not found.") ;
         }
@@ -113,4 +117,128 @@ exports.get_role = function(user_id, callback) {
             return callback(user.role) ;
         }
     })
-}
+};
+
+//Functions made for testing purposes
+//A function to statically create employees into the db
+exports.create_test_employees = function() {
+    console.log("creating a user");
+
+    var today = new Date();
+    module.exports.encrypt("test", function (enc_pass) {
+        var emp = {
+            _id: "emp1",
+            name: "employee1",
+            surname: "test",
+            password: enc_pass,
+            password_date: today,
+            email: "employee1@gmail.com",
+            role: "System test",
+            employment_length: 1,
+            skill: [],
+            current_projects: [],
+            past_projects: []
+        };
+
+        var emp2 = {
+            _id: "emp2",
+            name: "employee2",
+            surname: "test",
+            password: enc_pass,
+            password_date: today,
+            email: "employee2@gmail.com",
+            role: "System test",
+            employment_length: 1,
+            skill: [],
+            current_projects: [],
+            past_projects: []
+        };
+
+        var emp3 = {
+            _id: "emp3",
+            name: "employee3",
+            surname: "test",
+            password: enc_pass,
+            password_date: today,
+            email: "employee3@gmail.com",
+            role: "System test",
+            employment_length: 1,
+            skill: [],
+            current_projects: [],
+            past_projects: []
+        };
+
+        var emp4 = {
+            _id: "emp4",
+            name: "employee4",
+            surname: "test",
+            password: enc_pass,
+            password_date: today,
+            email: "employee4@gmail.com",
+            role: "System test",
+            employment_length: 1,
+            skill: [],
+            current_projects: [],
+            past_projects: []
+        };
+
+        var emp5 = {
+            _id: "emp5",
+            name: "employee5",
+            surname: "test",
+            password: enc_pass,
+            password_date: today,
+            email: "employee5@gmail.com",
+            role: "System test",
+            employment_length: 1,
+            skill: [],
+            current_projects: [],
+            past_projects: []
+        };
+        //dbs.remove({name: "Testy"});  /*THIS deletes all previous users in the db*/
+        module.exports.insertUser(emp);
+        module.exports.insertUser(emp2);
+        module.exports.insertUser(emp3);
+        module.exports.insertUser(emp4);
+        module.exports.insertUser(emp5);
+        console.log("employees added to data base")
+    });
+};
+
+//A function to statically remove employees from the db
+exports.remove_test_employees = function() {
+    console.log("removing users");
+    var user = schemas.user;
+    user.remove({role: "System test"}, function (err, result) {
+        if (err) {
+            console.log("Error removing users.");
+        }
+        else if (!result) {
+            console.log("database is empty.");
+        }
+        else {
+            console.log("Users removed");
+            console.log(JSON.stringify(result));
+        }
+    });
+};
+
+
+//A function to display the db in the terminal
+exports.view_employees = function()
+{
+    console.log("viewing all employees in user db");
+    var user = schemas.user;
+    user.find(function (err, result) {
+        if (err) {
+            console.log("Error displaying users.");
+        }
+        else if (!result) {
+            console.log("database is empty.");
+        }
+        else {
+            console.log("employees found");
+            console.log(JSON.stringify(result));
+        }
+    });
+};

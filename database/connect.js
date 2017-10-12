a5b010cb925decd1e42ddb0f2fcfcbe53381b811
Sir_Josh <josh.moodley09@gmail.com>
2017-10-12 23:06:26 +0200

    Added Docker files

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,28 @@
+// var exports = module.exports = {};
+//
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// exports.connect = function () {
+//     var url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
+//
+//     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
+//         if (err) {
+//             console.log("Connection to database failed.");
+//         }
+//         else {
+//             console.log("Connection to database established.");
+//
+//             exports.db = db;
+//         }
+//     });
+// };
+
 var exports = module.exports = {};
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 exports.connect = function () {
-    var url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
+    var url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
 
     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
         if (err) {

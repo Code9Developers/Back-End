69a833adbd9863b1ac90376e643286fc642be7b9
Sir_Josh <josh.moodley09@gmail.com>
2017-10-12 09:27:46 +0200

    Updated www and connect.js for Docker Automated Build.

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,34 @@
-var exports = module.exports = {};
+/**
+ * Local Build
+ */
+// let exports = module.exports = {};
+//
+// let mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// exports.connect = function () {
+//     let url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
+//
+//     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
+//         if (err) {
+//             console.log("Connection to database failed.");
+//         }
+//         else {
+//             console.log("Connection to database established.");
+//
+//             exports.db = db;
+//         }
+//     });
+// };
 
-var mongoose = require('mongoose');
+/**
+ * Docker Build
+ */
+let exports = module.exports = {};
+
+let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 exports.connect = function () {
-    var url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
+    let url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
 
     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
         if (err) {

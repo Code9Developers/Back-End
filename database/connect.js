4a06c79d2f26f80d39229c1e173dd8f5866b10b0
Seonin David <seonindavid@gmail.com>
2017-10-16 23:22:29 +0200

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,34 +1,12 @@
 /**
  * Local Build
  */
-// var exports = module.exports = {};
-//
-// var mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
-// exports.connect = function () {
-//     var url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
-//
-//     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
-//         if (err) {
-//             console.log("Connection to database failed.");
-//         }
-//         else {
-//             console.log("Connection to database established.");
-//
-//             exports.db = db;
-//         }
-//     });
-// };
-
-/**
- * Docker Build
- */
 var exports = module.exports = {};
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 exports.connect = function () {
-    var url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
+    var url = 'mongodb://127.0.0.1:27017/kpmg_dbs';
 
     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
         if (err) {
@@ -41,3 +19,25 @@ exports.connect = function () {
         }
     });
 };
+
+/**
+ * Docker Build
+ */
+// var exports = module.exports = {};
+
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// exports.connect = function () {
+//     var url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
+
+//     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
+//         if (err) {
+//             console.log("Connection to database failed.");
+//         }
+//         else {
+//             console.log("Connection to database established.");
+
+//             exports.db = db;
+//         }
+//     });
+// };

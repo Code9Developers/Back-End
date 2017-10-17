3c830efb5b9e7eaae267d65352285c402d9723ef
Sir_Josh <josh.moodley09@gmail.com>
2017-10-17 19:56:03 +0200

    SOMEBODY FIX THE LOGIN BEFORE I KILL SOMEONE

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 /**
  * Local Build
  */
- /*var exports = module.exports = {};
+ var exports = module.exports = {};
 
  var mongoose = require('mongoose');
  mongoose.Promise = global.Promise;
@@ -19,25 +19,25 @@
         }
     });
  };
-*/
+
 /**
  * Docker Build
  */
-var exports = module.exports = {};
-
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-exports.connect = function () {
-    var url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
-
-    mongoose.connect(url, {useMongoClient: true}, function (err, db) {
-        if (err) {
-            console.log("Connection to database failed.");
-        }
-        else {
-            console.log("Connection to database established.");
-
-            exports.db = db;
-        }
-    });
-};
+// var exports = module.exports = {};
+//
+// var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// exports.connect = function () {
+//     var url = 'mongodb://kpmg_dbs:27017/kpmg_dbs';
+//
+//     mongoose.connect(url, {useMongoClient: true}, function (err, db) {
+//         if (err) {
+//             console.log("Connection to database failed.");
+//         }
+//         else {
+//             console.log("Connection to database established.");
+//
+//             exports.db = db;
+//         }
+//     });
+// };
